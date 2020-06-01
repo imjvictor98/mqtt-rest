62871bc632c4729f7e30fb78ac392291d3ce01ce
Joao Victor Oliveira da Camara <joao.camara@firstdecision.com.br>
2020-06-01 14:14:04 -0300

    Updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser= require('body-parser')
-const mqttHandler = require('./MqttHandler')
+const mqttHandler = require('./mqttHandler')
 
 var app = express()
 
